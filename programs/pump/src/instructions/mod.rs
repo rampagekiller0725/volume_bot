84c353936520cde511c0cdcee52038ab6bfd98d2
rampagekiller0725 <rampagekiller0725@gmail.com>
2024-08-07 09:33:41 -0700

    create raydium pool remove

--- programs/pump/src/instructions/mod.rs
+++ programs/pump/src/instructions/mod.rs
@@ -2,10 +2,10 @@ pub mod add_liquidity;
 pub mod initialize;
 pub mod remove_liquidity;
 pub mod swap;
-pub mod create_raydium_pool;
+// pub mod create_raydium_pool;
 
 pub use add_liquidity::*;
 pub use initialize::*;
 pub use remove_liquidity::*;
 pub use swap::*;
-pub use create_raydium_pool::*;
+// pub use create_raydium_pool::*;
